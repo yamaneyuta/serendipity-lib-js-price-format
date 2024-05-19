59d10db6f5b88f99fb896ef63cd292ed70777632
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-05-19 16:03:36 +0900

    小数点の区切り文字を取得する関数追加

---
+++ tests/getDecimalSeparator.test.ts
@@ -0,0 +1,31 @@
+import { getDecimalSeparator } from "../src/functions/getDecimalSeparator";
+
+// https://ja.wikipedia.org/wiki/%E5%B0%8F%E6%95%B0%E7%82%B9
+// > 小数点として点（point on the line）を用いるか、コンマ（comma on the line）を用いるかは、国、地域、文化によってまちまちである。
+// > ごく大まかには、イギリス、米国、日本、中国、インドでは「点」を用い、
+// > フランス、ドイツ、イタリア、スペイン、ロシアでは「コンマ」を用いる。
+
+test.each`
+    locales    | expected
+    ${"en-GB"} | ${"."}
+    ${"en"}    | ${"."}
+    ${"en-US"} | ${"."}
+    ${"ja-JP"} | ${"."}
+    ${"ja"}    | ${"."}
+    ${"zh-CN"} | ${"."}
+    ${"zh"}    | ${"."}
+
+    ${"fr-FR"} | ${","}
+    ${"fr"}    | ${","}
+    ${"de-DE"} | ${","}
+    ${"de"}    | ${","}
+    ${"it-IT"} | ${","}
+    ${"it"}    | ${","}
+    ${"es-ES"} | ${","}
+    ${"es"}    | ${","}
+    ${"ru-RU"} | ${","}
+    ${"ru"}    | ${","}
+`('getDecimalSeparator($locales) === $expected', ({ locales, expected }) => {
+    expect(getDecimalSeparator(locales)).toBe(expected);
+});
+    
