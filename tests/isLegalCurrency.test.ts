77fafd6f324df72bd297a0c5bf53dbd058617927
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-05-19 21:27:50 +0900

    テスト一覧の書式を整形

--- tests/isLegalCurrency.test.ts
+++ tests/isLegalCurrency.test.ts
@@ -1,31 +1,30 @@
 import { isLegalCurrency } from "../src/functions/isLegalCurrency";
 
 test.each`
-    symbol    | locales    | expected
+    symbol    | locales      | expected
     ${"JPY"}  | ${undefined} | ${true}
-    ${"JPY"}  | ${"en-US"} | ${true}
-    ${"JPY"}  | ${"ja-JP"} | ${true}
+    ${"JPY"}  | ${"en-US"}   | ${true}
+    ${"JPY"}  | ${"ja-JP"}   | ${true}
 
     ${"USD"}  | ${undefined} | ${true}
-    ${"USD"}  | ${"en-US"} | ${true}
-    ${"USD"}  | ${"ja-JP"} | ${true}
+    ${"USD"}  | ${"en-US"}   | ${true}
+    ${"USD"}  | ${"ja-JP"}   | ${true}
 
     ${"EUR"}  | ${undefined} | ${true}
-    ${"EUR"}  | ${"en-US"} | ${true}
-    ${"EUR"}  | ${"ja-JP"} | ${true}
+    ${"EUR"}  | ${"en-US"}   | ${true}
+    ${"EUR"}  | ${"ja-JP"}   | ${true}
 
     ${"ETH"}  | ${undefined} | ${false}
-    ${"ETH"}  | ${"en-US"} | ${false}
-    ${"ETH"}  | ${"ja-JP"} | ${false}
+    ${"ETH"}  | ${"en-US"}   | ${false}
+    ${"ETH"}  | ${"ja-JP"}   | ${false}
 
     ${"USDC"} | ${undefined} | ${false}
-    ${"USDC"} | ${"ja-JP"} | ${false}
-    ${"USDC"} | ${"en-US"} | ${false}
+    ${"USDC"} | ${"ja-JP"}   | ${false}
+    ${"USDC"} | ${"en-US"}   | ${false}
 
-    ${"FOO"} | ${undefined} | ${false}
-    ${"FOO"} | ${"ja-JP"} | ${false}
-    ${"FOO"} | ${"en-US"} | ${false}
-
+    ${"FOO"}  | ${undefined} | ${false}
+    ${"FOO"}  | ${"ja-JP"}   | ${false}
+    ${"FOO"}  | ${"en-US"}   | ${false}
 `('isLegalCurrency($symbol, $locales) === $expected', ({ symbol, locales, expected }) => {
     expect(isLegalCurrency(symbol, locales)).toBe(expected);
 });
